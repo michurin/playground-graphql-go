67d6b85bb353e6bdc0dd33913c331d3ed0b4040e
Alexey Michurin <michurin@gett.com>
2018-09-18 18:42:46 +0300

    Add DB

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/graphql-go/graphql"
+	"github.com/mxk/go-sqlite/sqlite3"
 	gqlhandler "github.com/graphql-go/graphql-go-handler"
+	"net/http"
 )
 
 // TODO:
@@ -13,6 +13,31 @@ import (
 // + arguments
 // - args from parent nodes
 // - cache?.. recursion?..
+// - make shared DB adapter
+
+func sql(sql string) []sqlite3.RowMap {
+	var result []sqlite3.RowMap
+	c, err := sqlite3.Open("database.db")
+	if err != nil {
+		panic(err)
+	}
+	s, err := c.Query(sql)
+	if err != nil {
+		return result
+	}
+	for {
+		row := make(sqlite3.RowMap)
+		s.Scan(row)
+		result = append(result, row)
+		fmt.Printf("%#v\n", row)
+		err = s.Next()
+		if err != nil {
+			break
+		}
+	}
+	fmt.Println(result)
+	return result
+}
 
 type Ride struct {
 	Id   int
@@ -46,6 +71,20 @@ var RideType = graphql.NewObject(graphql.ObjectConfig{
 	},
 })
 
+
+type Customer struct { // TODO: add rides!
+	Id int
+	Name string
+}
+
+var CustomerType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Customer",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{Type: graphql.Int},
+		"name": &graphql.Field{Type: graphql.String},
+	},
+})
+
 func main() {
 	fields := graphql.Fields{
 		"lastRide": &graphql.Field{
@@ -58,6 +97,25 @@ func main() {
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 				fmt.Printf("lastRide resolver params: %+v\n", p.Args)
 				return Ride{1, Driver{100, ""}}, nil
+			},
+		},
+		"customer": &graphql.Field{
+			Type: CustomerType,
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				customerId := p.Args["id"] // TODO: or from context?!
+				res :=  sql(fmt.Sprintf("select * from Customer where customer_id=%d", customerId))
+				fmt.Printf("customer resolver params: %+v\n", p.Args)
+				fmt.Printf("%+v\n", res)
+				if len(res) == 0 {
+					return nil, nil
+				}
+				fmt.Printf("type=%T\n", res[0]["customer_id"])
+				return Customer{int(res[0]["customer_id"].(int64)), res[0]["name"].(string)}, nil
 			},
 		},
 	}
