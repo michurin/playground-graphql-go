30be4bbfd12370db4e7b72fb478dafd020f0872a
Alexey Michurin <michurin@gett.com>
2018-09-21 11:35:38 +0300

    Improve DB init script

--- database.sh
+++ database.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
 cat <<__END__ | sqlite3 database.db
-drop table if exists Ride;
-drop table if exists Driver;
-drop table if exists Customer;
-create table Ride (ride_id int, driver_id int, customer_id int, destination string);
-create table Driver (driver_id int, name string);
-create table Customer (customer_id int, name string);
-
-insert into Customer values
-  (100, "Customer_100"),
-  (200, "Customer_200");
-insert into Driver values
-  (1, "Driver_1"),
-  (2, "Driver_2");
-insert into Ride values
-  (1, 1, 100, "Adderss_for_ride_1"),
-  (2, 1, 200, "Address_for_ride_2"),
-  (3, 2, 200, "Address_for_ride_3");
-select * from Ride join Driver using (driver_id) join Customer using (customer_id);
--- 1|1|100|R1|D1|C100
--- 2|1|200|R2|D1|C200
--- 3|2|200|R3|D2|C200
+PRAGMA foreign_keys=ON;
+BEGIN TRANSACTION;
+DROP TABLE IF EXISTS Ride;
+DROP TABLE IF EXISTS Driver;
+DROP TABLE IF EXISTS Customer;
+CREATE TABLE Driver (
+  driver_id integer primary key autoincrement,
+  name string);
+insert INTO Driver VALUES(1,'Driver_1');
+insert INTO Driver VALUES(2,'Driver_2');
+CREATE TABLE Customer (
+  customer_id integer primary key autoincrement,
+  name string);
+insert INTO Customer VALUES(100,'Customer_100');
+insert INTO Customer VALUES(200,'Customer_200');
+CREATE TABLE Ride (
+  ride_id integer primary key autoincrement,
+  driver_id integer references Driver,
+  customer_id integer references Customer,
+  destination string);
+insert INTO Ride VALUES(1,1,100,'Adderss_for_ride_1');
+insert INTO Ride VALUES(2,1,200,'Address_for_ride_2');
+insert INTO Ride VALUES(3,2,200,'Address_for_ride_3');
+COMMIT;
 __END__
